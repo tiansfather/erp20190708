e66e535821b316753af8b4b4fddcc6bc15e0c89b
Tiansfather <Tiansfather@TIANSFATHER>
2019-07-12 16:13:53 +0800

    1

--- src/Master.Application/Storage/MaterialDIYAppService.cs
+++ src/Master.Application/Storage/MaterialDIYAppService.cs
@@ -3,6 +3,7 @@ using Master.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,7 +22,17 @@ namespace Master.Storage
             return (await base.GetQueryable(request))
                 .Where(o=>o.MaterialDIYType==MaterialDIYType.组装 && o.MaterialNature==MaterialNature.实物);
         }
+        protected override async Task<IQueryable<Material>> BuildSearchQueryAsync(IDictionary<string, string> searchKeys, IQueryable<Material> query)
+        {
+            query=await base.BuildSearchQueryAsync(searchKeys, query);
 
+            if (searchKeys.ContainsKey("materialDIYType"))
+            {
+                query = query.Where($"materialDIYType={searchKeys["materialDIYType"]}");
+            }
+
+            return query;
+        }
         /// <summary>
         /// 获取某商品的组装信息
         /// </summary>
@@ -46,6 +57,16 @@ namespace Master.Storage
                     Number=diyInfo.First(d=>d.MaterialId==o.Id).Number
                 })
             };
+        }
+        /// <summary>
+        /// 设置组装信息
+        /// </summary>
+        /// <param name="materialDIYSubmitDto"></param>
+        /// <returns></returns>
+        public virtual async Task SetMaterialDiyInfo(MaterialDIYSubmitDto materialDIYSubmitDto)
+        {
+            var material = await Manager.GetByIdAsync(materialDIYSubmitDto.Id);
+            material.DIYInfo = materialDIYSubmitDto.DIYInfos.ToList();
         }
     }
 }
---
+++ src/Master.Application/Storage/MaterialDIYSubmitDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Master.Storage
+{
+    public class MaterialDIYSubmitDto
+    {
+        public int Id { get; set; }
+        public IEnumerable<DIYInfo> DIYInfos { get; set; }
+    }
+}
